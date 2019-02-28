18a0fd3f6bc1fb18ccc6517f6a6d6f5bce48f758
Michellelok <hmichellelok@gmail.com>
2019-02-28 00:09:30 -0500

    Michelle's homepage XML

---
+++ app/src/main/java/com/example/cis350app/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.cis350app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
