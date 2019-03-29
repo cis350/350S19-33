c8625f7e63ffa1823c4fb36170bc4573e90afa38
Dorothy Chang <dorchang@seas.upenn.edu>
2019-03-29 12:25:02 -0400

    mongo

--- person-app/Person.js
+++ person-app/Person.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // the host:port must match the location where you are running MongoDB
 // the "myDatabase" part can be anything you like
-mongoose.connect('mongodb://localhost:27017/myDatabase');
+mongoose.connect('mongodb+srv://dorothy:dorothy@cluster0-8qbcz.mongodb.net/test?retryWrites=true');
 
 var Schema = mongoose.Schema;
 
