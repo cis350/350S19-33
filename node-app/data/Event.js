4956cd345bba44d0fb0fa9adae19c82d8a5748f6
Chelsey Lin <chelseyl@sas.upenn.edu>
2019-03-31 22:13:14 -0400

    Event schema

---
+++ node-app/data/Event.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://dorothy:dorothy@cluster0-8qbcz.mongodb.net/test?retryWrites=true');
+
+var Schema = mongoose.Schema;
+
+var eventSchema = new Schema({
+	name: {type: String, required: true, unique: true},
+	location: {type: String, required: true},
+	time: {type: String, required: true},
+	host: {type: String, required: true},
+	description: {type: String, required: true}
+    });
+
+module.exports = mongoose.model('Event', eventSchema);
