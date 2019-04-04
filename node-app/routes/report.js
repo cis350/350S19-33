b0c16c652db654af9f908f923c78a4de06fdb6b9
Michellelok <hmichellelok@gmail.com>
2019-04-04 17:16:27 -0400

    report fixed

--- node-app/routes/report.js
+++ node-app/routes/report.js
@@ -4,21 +4,29 @@ const Report = require('../data/Report.js');
 const Memo = require('../data/Memo.js');
 const ObjectId = require('mongodb').ObjectID;
 
-
 const getReports = function(req, res) {
-    const person = req.session.user;
-        Report.find((err, reports) => {
-            if (err) {
-                res.type('html').status(500);
-                res.send('Error: ' + err);
-            } else if (reports.length == 0) {
-                res.type('html').status(200);
-                res.send('There are no reports');
-            } else {
-                res.render('reports.ejs', { reports: reports, person: req.session.user });
-            }
-        });
-    };
+  const person = req.session.user;
+
+  Report.find((err, reports) => {
+    if(!req.session.user){
+      res.redirect('login/');
+    } else if (err) { 
+      res.type('html').status(500);
+      res.send('Error: ' + err); 
+    } else if (reports.length == 0) {
+      res.type('html').status(200);
+      res.send('There are no reports');
+    } else {
+      var finalReports = [];
+        reports.forEach(async (item) => {
+          if(item.adminEmail == person.email){
+            finalReports.push(item);
+        }
+      });
+      res.render('reports.ejs', { reports: finalReports, person: req.session.user });
+    }
+});
+};
 
 const getReport = function(req, res){
    const id = req.query._id;
@@ -124,33 +132,50 @@ const showMemos = function(req, res){
 
 
 const getRead = function(req, res) {
-    const person = req.session.user;
-    Report.find((err, reports) => {
-        if (err) {
-            res.type('html').status(500);
-            res.send('Error: ' + err);
-        } else if (reports.length == 0) {
-            res.type('html').status(200);
-            res.send('There are no reports');
-        } else {
-            res.render('reports.ejs', { reports: reports, person: req.session.user });
+  const person = req.session.user;
+  console.log(person);
+  Report.find((err, reports) => {
+    if(!req.session.user){
+      res.redirect('login/');
+    } else if (err) { 
+      res.type('html').status(500);
+      res.send('Error: ' + err); 
+    } else if (reports.length == 0) {
+      res.type('html').status(200);
+      res.send('There are no reports');
+    } else {
+      var finalReports = [];
+        reports.forEach(async (item) => {
+          if(item.adminEmail == person.email && item.read){
+            finalReports.push(item);
         }
-    });
+      });
+      res.render('reports.ejs', { reports: finalReports, person: req.session.user });
+    }
+});
 };
 
 const getUnread = function(req, res) {
-    const person = req.session.user;
-    Report.find((err, reports) => {
-        if (err) {
-            res.type('html').status(500);
-            res.send('Error: ' + err);
-        } else if (reports.length == 0) {
-            res.type('html').status(200);
-            res.send('There are no reports');
-        } else {
-             res.render('reports.ejs', { reports: reports, person: req.session.user });
+  const person = req.session.user;
+  Report.find((err, reports) => {
+    if(!req.session.user){
+      res.redirect('login/');
+    } else if (err) { 
+      res.type('html').status(500);
+      res.send('Error: ' + err); 
+    } else if (reports.length == 0) {
+      res.type('html').status(200);
+      res.send('There are no reports');
+    } else {
+      var finalReports = [];
+        reports.forEach(async (item) => {
+          if(item.adminEmail == person.email && !item.read){
+            finalReports.push(item);
         }
-    });
+      });
+      res.render('reports.ejs', { reports: finalReports, person: req.session.user });
+    }
+});
 };
 
 const routes = {
