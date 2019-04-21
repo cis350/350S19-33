0f5de48d67e3388f9b7bb5aed4c46ad8b2fcd540
Chelsey Lin <chelseyl@sas.upenn.edu>
2019-04-21 01:16:52 -0400

    remove commented out code

--- app/src/main/java/com/example/cis350app/EventListActivity.java
+++ app/src/main/java/com/example/cis350app/EventListActivity.java
@@ -119,36 +119,6 @@ public class EventListActivity extends AppCompatActivity {
         return ITEM_MAP.get(name);
     }
 
-    /*
-
-
-        List<EventContent.Event> registeredEvents = getRegistered().get(0);
-        List<EventContent.Event> unRegisteredEvents = getRegistered().get(1);
-
-        registeredList.setAdapter(new ArrayAdapter<EventContent.Event>(this, android.R.layout.simple_list_item_1, registeredEvents));
-        unregisteredList.setAdapter(new ArrayAdapter<EventContent.Event>(this, android.R.layout.simple_list_item_1, unRegisteredEvents));
-
-        ListUtils.setDynamicHeight(registeredList);
-        ListUtils.setDynamicHeight(unregisteredList);
-
-        registeredList.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        EventContent.Event item = (EventContent.Event) view.getTag();
-                        Context context = view.getContext();
-                        Intent intent = new Intent(context, EventDetailActivity.class);
-                        intent.putExtra(EventDetailFragment.ARG_ITEM_ID, EventContent.ITEMS.get(i).id);
-                        context.startActivity(intent);
-                    }
-
-                }
-
-        );
-    }
-
-    */
-
     /**
      * Fetch notifications
      */
@@ -189,44 +159,4 @@ public class EventListActivity extends AppCompatActivity {
             }
         }
     }
-
-/*
-    public static class ListUtils {
-        public static void setDynamicHeight(ListView mListView) {
-            ListAdapter mListAdapter = mListView.getAdapter();
-            if (mListAdapter == null) {
-                // when adapter is null
-                return;
-            }
-            int height = 0;
-            int desiredWidth = MeasureSpec.makeMeasureSpec(mListView.getWidth(), MeasureSpec.UNSPECIFIED);
-            for (int i = 0; i < mListAdapter.getCount(); i++) {
-                View listItem = mListAdapter.getView(i, null, mListView);
-                listItem.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
-                height += listItem.getMeasuredHeight();
-            }
-            ViewGroup.LayoutParams params = mListView.getLayoutParams();
-            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
-            mListView.setLayoutParams(params);
-            mListView.requestLayout();
-        }
-    }
-
-    private List<List<EventContent.Event>> getRegistered() {
-        List<EventContent.Event> registeredEvents = new ArrayList<EventContent.Event>();
-        List<EventContent.Event> unregisteredEvents = new ArrayList<EventContent.Event>();
-        List<EventContent.Event> events = EventContent.ITEMS;
-        for (int i = 0; i < events.size(); i++) {
-            if (events.get(i).isRegistered()) {
-                registeredEvents.add(events.get(i));
-            } else {
-                unregisteredEvents.add(events.get(i));
-            }
-        }
-        List<List<EventContent.Event>> partitionedEvents = new ArrayList<List<EventContent.Event>>();
-        partitionedEvents.add(registeredEvents);
-        partitionedEvents.add(unregisteredEvents);
-        return partitionedEvents;
-    }
-    */
 }
